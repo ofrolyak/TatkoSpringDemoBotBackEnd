18a6f16735488b2162990da1b7a0ec416d274e6d
Oleg Frolyak <ofrolyak@gmail.com>
2024-07-20 16:30:48 +0300

    Logs and something else

--- src/main/java/com/tatko/tatkospringdemobotbackend/config/TelegramBotInitializer.java
+++ src/main/java/com/tatko/tatkospringdemobotbackend/config/TelegramBotInitializer.java
@@ -1,6 +1,7 @@
 package com.tatko.tatkospringdemobotbackend.config;
 
 import com.tatko.tatkospringdemobotbackend.service.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -9,6 +10,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Slf4j
 @Component
 public class TelegramBotInitializer {
 
@@ -16,11 +18,16 @@ public class TelegramBotInitializer {
     TelegramBot telegramBot;
 
     @EventListener({ContextRefreshedEvent.class})
-    public void init() throws TelegramApiException {
+    public void init() {
 
+        log.info("Initializing TelegramBot");
+        try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-
             telegramBotsApi.registerBot(telegramBot);
+        } catch (TelegramApiException e) {
+            log.error("Initializing TelegramBot Error: ", e);
+        }
+        log.info("TelegramBot initialized");
     }
 
 }
