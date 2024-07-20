33febe6659107710fe1b2dabb8a13e332476f8e8
Oleg Frolyak <ofrolyak@gmail.com>
2024-07-20 13:15:32 +0300

    Spring Init project

---
+++ src/test/java/com/tatko/TatkoSpringDemoBotBackEnd/TatkoSpringDemoBotBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatko.TatkoSpringDemoBotBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatkoSpringDemoBotBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
