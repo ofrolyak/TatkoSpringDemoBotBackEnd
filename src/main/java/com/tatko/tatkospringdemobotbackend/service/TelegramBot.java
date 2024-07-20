53fdbf9612c83466dba088c2d54db8cb07d06041
Oleg Frolyak <ofrolyak@gmail.com>
2024-07-20 20:01:36 +0300

    Added some use cases for menu

--- src/main/java/com/tatko/tatkospringdemobotbackend/service/TelegramBot.java
+++ src/main/java/com/tatko/tatkospringdemobotbackend/service/TelegramBot.java
@@ -1,6 +1,7 @@
 package com.tatko.tatkospringdemobotbackend.service;
 
 import com.tatko.tatkospringdemobotbackend.config.TelegramBotConfig;
+import jakarta.annotation.PostConstruct;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,15 @@ import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Slf4j
@@ -18,6 +25,12 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Autowired
     TelegramBotConfig telegramBotConfig;
+
+    @PostConstruct
+    public void postConstructInit(){
+
+        addCommandToBot();
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -34,12 +47,26 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/start":
                     startCommandReceived(chatId, firstName);
                     break;
+                case "/help":
+                    sendMessage(chatId, "This is bot for demonstration how to Spring Boot works with Telegram.");
+                    break;
                 default:
                     sendMessage(chatId, "Головне в нашому житті - не тупікувати!!!");
                     break;
             }
         }
 
+    }
+
+    @SneakyThrows
+    void addCommandToBot() {
+        List<BotCommand> botCommandList = new ArrayList<>();
+        botCommandList.add(new BotCommand("/start", "get welcome message"));
+        botCommandList.add(new BotCommand("/getmydata", "get your data"));
+        botCommandList.add(new BotCommand("/deletemydata", "delete your data"));
+        botCommandList.add(new BotCommand("/help", "how to use this bot"));
+        botCommandList.add(new BotCommand("/settings", "settings and preferences"));
+        this.execute(new SetMyCommands(botCommandList, new BotCommandScopeDefault(), null));
     }
 
     void startCommandReceived(long chatId, String name) {
