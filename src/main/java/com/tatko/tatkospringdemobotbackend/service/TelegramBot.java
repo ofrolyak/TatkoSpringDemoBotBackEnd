0b0199ea4d38ee80452850daa0d1feb1f4bdd738
Oleg Frolyak <ofrolyak@gmail.com>
2024-07-25 12:16:57 +0300

    Some issue for Codacy 1

--- src/main/java/com/tatko/tatkospringdemobotbackend/service/TelegramBot.java
+++ src/main/java/com/tatko/tatkospringdemobotbackend/service/TelegramBot.java
@@ -36,7 +36,7 @@ import java.util.Set;
 public class TelegramBot extends TelegramLongPollingBot {
 
     /**
-     * TelegramBotConfig itself.
+     * TelegramBotConfig itself .
      */
     @Autowired
     private TelegramBotConfig telegramBotConfig;
